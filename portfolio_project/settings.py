5e2230ff4cb0c59e856eac4cfd201c86b6e28dbc
Pradeeptadi <pradeeptadi03@gmail.com>
2025-07-03 17:08:31 +0530

    fix: set STATIC_ROOT for render deployment

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -11,6 +11,18 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+# This tells Django where to collect static files
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # for production
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')  # for development
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
